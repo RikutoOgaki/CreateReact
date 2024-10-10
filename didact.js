8dc2e3e0f35f0e88900cfbed456f3e24ba1ef3c4
Rikuto <orikutobasuke@gmail.com>
2024-10-10 10:35:31 +0900

    progress: step2 completion

--- didact.js
+++ didact.js
@@ -23,7 +23,23 @@ function createTextElemetn(text) {
 }
 
 function render(element, container) {
-    //TODO create dom nodes
+    const dom =
+        element.type == "TEXT_ELEMENT"
+            ? document.createElement("")
+            : document.createElement(element.type)
+
+    const isProperty = key => key !== "children"
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = element.props[name]
+        })
+
+    element.props.children.forEach(child =>
+        render(child, dom)
+    )
+
+    container.appendChild(dom)
 }
 
 const Didact = {
